175b53ade0542dd6de5931aa921396e0930c0f4e
Ziqi Wang <wangziqi000@126.com>
2019-11-15 16:04:25 -0800

    read dataset

---
+++ main.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from read_frame_data import read_uwb_data
+
+# read the audio-UWB dataset
+filepath = "/home/ziqi/Desktop/collected_data_191109"
+uwb_audio_dataset = read_uwb_data(filepath)
+
+# The next step is to perform data collection
+
+# The next step is find the "range of interest", i.e. locate the speaker
+    # Ziqi's proposal: Calculate the phase and amplitude of each distance, and then calculate variance of them
+
+# The next step is to plot the phase and amplitude over time, and perform STFT on them
+
+
+print("NESL is awesome!")
--- read_frame_data.py
+++ read_frame_data.py
@@ -1,35 +1,88 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import re
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
-data_files = os.listdir("/home/ziqi/Desktop/collected_data_191109")
-for each in data_files:
-	file_info = each.split("_")
-	if breathing not in file_info:
-# fp = open("sample_data.txt", "r+")
-text_content = fp.readlines()
-fp.close()
+def read_uwb_data(filepath):
+	data_files = os.listdir(filepath)
+	uwb_data = {}
+	for filename in data_files:
+		file_info = filename.split("_")
+		if "breathing" not in file_info:
 
-data_frames = []
+			audio_frequency = int(file_info[0]) + 0.01 * int(file_info[1])
 
-for data_line in text_content:
-	if ("{" not in data_line) and ("}" not in data_line):
-		continue
-	else:
-		data_points = re.split(",|{|}", data_line.strip())[2:-2]
-		data_points = [float(data_points[i]) for i in range(0, len(data_points))]
-		real = [data_points[i] for i in range(0, len(data_points)) if i % 2 == 0]
-		imag = [data_points[i] for i in range(0, len(data_points)) if i % 2 == 1]
-		complex_tuples = list(zip(real, imag))
-		data_points = [complex(each[0], each[1]) for each in complex_tuples]
-		data_frames.append(data_points)
+			fp = open("sample_data.txt", "r+")
+			text_content = fp.readlines()
+			fp.close()
 
-plt.figure()
-plt.xticks(np.arange(0, 2.5, step=0.1))
+			data_frames = []
+
+			for data_line in text_content:
+				if ("{" not in data_line) and ("}" not in data_line):
+					continue
+				else:
+					data_points = re.split(",|{|}", data_line.strip())[2:-2]
+					data_points = [float(data_points[i]) for i in range(0, len(data_points))]
+					real = [data_points[i] for i in range(0, len(data_points)) if i % 2 == 0]
+					imag = [data_points[i] for i in range(0, len(data_points)) if i % 2 == 1]
+					complex_tuples = list(zip(real, imag))
+					data_points = [complex(each[0], each[1]) for each in complex_tuples]
+					data_frames.append(data_points)
+
+			if audio_frequency not in uwb_data.keys():
+				uwb_data[audio_frequency] = []
+			uwb_data[audio_frequency].append(data_frames)
+			print("Loaded data of audio frequency {}, Count {}.".format(audio_frequency, len(uwb_data[audio_frequency])))
+	print("Reading data done.")
+	return uwb_data
+
+
+def read_breath_data(filepath):
+	data_files = os.listdir(filepath)
+	uwb_data = {}
+	for filename in data_files:
+		file_info = filename.split("_")
+		if "breathing" in file_info:
+
+			testee = file_info[1]
+			# distance = file_info[2]
+
+			fp = open("sample_data.txt", "r+")
+			text_content = fp.readlines()
+			fp.close()
+
+			data_frames = []
+
+			for data_line in text_content:
+				if ("{" not in data_line) and ("}" not in data_line):
+					continue
+				else:
+					data_points = re.split(",|{|}", data_line.strip())[2:-2]
+					data_points = [float(data_points[i]) for i in range(0, len(data_points))]
+					real = [data_points[i] for i in range(0, len(data_points)) if i % 2 == 0]
+					imag = [data_points[i] for i in range(0, len(data_points)) if i % 2 == 1]
+					complex_tuples = list(zip(real, imag))
+					data_points = [complex(each[0], each[1]) for each in complex_tuples]
+					data_frames.append(data_points)
+
+			if testee not in uwb_data.keys():
+				uwb_data[testee] = data_frames
+			print("Loaded data of testee {}.".format(testee))
+	print("Reading data done.")
+	return uwb_data
+
+# # plot the phase and amplitude
+# plt.figure()
+# plt.xticks(np.arange(0, 2.5, step=0.1))
 
 # sample_data = data_frames[500]
 # sample_data_amp = [abs(each) for each in sample_data]
 # plt.plot(np.arange(0, 2.5, step=0.05), sample_data_amp)
 
-plt.show()
+# plt.show()
+
+
+if __name__ == "__main__":
+	breath_data = read_breath_data("/home/ziqi/Desktop/collected_data_191109")
+	print("NESL is awesome.")
